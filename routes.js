a1d85498253ad003c9c343d0ee512760067febae
Talamantez <robert.talamantez2@gmail.com>
2014-12-30 09:58:05 -0800

    initial push

---
+++ routes.js
@@ -0,0 +1,51 @@
+var passport = require('passport');
+var Account = require('./models/account');
+
+module.exports = function (app) {
+
+  app.get('/', function (req, res) {
+      res.render('index', { user : req.user });
+  });
+
+  app.get('/register', function(req, res) {
+      res.render('register', { });
+  });
+
+  app.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { info : "account already exists" });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+          res.redirect('/');
+        });
+    });
+  });
+
+  app.get('/login', function(req, res) {
+      res.render('login', { user : req.user });
+  });
+
+  app.post('/login',function(req, res, next) {
+     passport.authenticate('local', function(err, user, info, info1, link){
+        if ( err ) { return next(err); }
+        if ( !user ) { return res.render('login', {info : "That user doesn't exist yet. You can register ", info1: "?",link: "register"});
+          }
+        req.logIn(user, function(err) {
+          if (err){ return next(err); }
+          return res.redirect('/');
+        });
+     })(req, res, next);
+  });
+
+  app.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+  });
+
+  app.get('/ping', function(req, res){
+      res.send("pong!", 200);
+  });
+
+};
